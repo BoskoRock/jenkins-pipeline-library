eff7083e3682a643ac1124328ae3e93daebe830d
BoskoRock <102969749+BoskoRock@users.noreply.github.com>
2022-04-14 16:28:51 +0200

    Update deploymentScript.sh

--- jenkins-script/deploymentScript.sh
+++ jenkins-script/deploymentScript.sh
@@ -1,8 +1,11 @@
 node {
-    // This displays colors using the 'xterm' ansi color map.
-    ansiColor('xterm') {
-        // Just some echoes to show the ANSI color.
-        stage "\u001B[31mI'm Red\u001B[0m Now not"
-		println("Izvrsio sam")
-    }
+  {
+  	def bool = env.isHuman
+	if(bool == 'true'){
+		println("Hello " + env.Human)
+	} else {
+		println("Hello world")
+		}
+  }
+    
 }
