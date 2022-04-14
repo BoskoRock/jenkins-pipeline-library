3c16be7593a72de2514fec28576505c6ffca7e28
BoskoRock <nemanja.boshkovic1987@gmail.com>
2022-04-14 16:42:49 +0200

    Added clone for webgoat

--- jenkins-script/deploymentScript.sh
+++ jenkins-script/deploymentScript.sh
@@ -1,10 +1,15 @@
 node {
   
-  	def bool = env.isHuman
-	if(bool == 'true'){
-		println("Hello " + env.Human)
-	} else {
-		println("Hello world")
-		}
+  	//def bool = env.isHuman
+	//if(bool == 'true'){
+	//	println("Hello " + env.Human)
+	//} else {
+	//	println("Hello world")
+	//	}
+	
+	stage("Clone WebGoat repo"){
+		sh "git clone https://github.com/BoskoRock/WebGoat.git"
+		sh "ls"
+	}
     
 }
