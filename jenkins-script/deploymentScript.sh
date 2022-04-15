997a3206aaf0def07a9d510c7502e0f9debc8d8f
BoskoRock <102969749+BoskoRock@users.noreply.github.com>
2022-04-15 11:07:52 +0200

    Update deploymentScript.sh

--- jenkins-script/deploymentScript.sh
+++ jenkins-script/deploymentScript.sh
@@ -1,24 +1,20 @@
-node {
-  
-  	//def bool = env.isHuman
-	//if(bool == 'true'){
-	//	println("Hello " + env.Human)
-	//} else {
-	//	println("Hello world")
-	//	}
-	
+node {	
 	stage("Clone WebGoat repo"){
 		sh "rm -r WebGoat"
 		sh "git clone https://github.com/BoskoRock/WebGoat.git"
 		sh "ls"
 	}
 	stage("Build WebGoat application"){
-	        sh "export MAVEN_HOME=/usr/share/maven"
-                sh "export PATH=$PATH:/usr/share/maven/bin"
-                sh "mvn --version"
-		sh "cd WebGoat"
-		sh "ls"
-	    sh "mvn clean install"
+		// build Webgoat using maven
+	}
+	stage("Run tests"){
+		// run unit tests
+	}
+	stage("Push image"){
+		// push image to public docker-hub
+	}
+	stage("Deploy to swarm"){
+		// deploy to swarm
 	}
     
 }
