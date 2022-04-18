c9bcf1fe281b85b68d0d3b260b7601f56250bef7
BoskoRock <102969749+BoskoRock@users.noreply.github.com>
2022-04-18 14:06:06 +0200

    Update deploymentScript.sh

--- jenkins-script/deploymentScript.sh
+++ jenkins-script/deploymentScript.sh
@@ -4,26 +4,32 @@ pipeline {
 		maven 'Maven 3.8.5' 
         	jdk 'jdk8'
 	}
-	stage("Clone WebGoat repo"){
-		sh "rm -r WebGoat"
-		sh "git clone https://github.com/BoskoRock/WebGoat.git"
-		sh "ls"
-	}
-	stage("Build WebGoat application"){
-		dir("${env.WORKSPACE}/WebGoat"){
-   			 sh "pwd"
-			 sh "ls"
-			 sh "mvn clean install"
+	stages{
+		stage("Clone WebGoat repo"){
+			steps{
+				sh "rm -r WebGoat"
+				sh "git clone https://github.com/BoskoRock/WebGoat.git"
+				sh "ls"
+			}
 		}
-	}
-	stage("Run tests"){
-		// run unit tests
-	}
-	stage("Push image"){
-		// push image to public docker-hub
-	}
-	stage("Deploy to swarm"){
-		// deploy to swarm
+		stage("Build WebGoat application"){
+			steps{
+				dir("${env.WORKSPACE}/WebGoat"){
+					 sh "pwd"
+					 sh "ls"
+					 sh "mvn clean install"
+				}
+			}
+		}
+		stage("Run tests"){
+			// run unit tests
+		}
+		stage("Push image"){
+			// push image to public docker-hub
+		}
+		stage("Deploy to swarm"){
+			// deploy to swarm
+		}
 	}
     
 }
