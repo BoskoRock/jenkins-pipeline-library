7c45ada4cbf2fa0d7dc96b374e48ed391afc78a4
BoskoRock <102969749+BoskoRock@users.noreply.github.com>
2022-05-11 17:01:45 +0200

    Update deploymentScript.sh

--- jenkins-script/deploymentScript.sh
+++ jenkins-script/deploymentScript.sh
@@ -11,7 +11,7 @@ pipeline {
 	stages{
 		stage("Clone WebGoat repo"){
 			steps{
-				//sh "rm -r maven-simple"
+				sh "rm -r maven-simple"
 				sh "java -version"
 				sh "git clone https://github.com/jitpack/maven-simple.git"
 				sh "ls"
