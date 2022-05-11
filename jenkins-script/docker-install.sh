bd93a1ca70c3300f00f9107651828d26bf54fc1a
BoskoRock <102969749+BoskoRock@users.noreply.github.com>
2022-05-11 16:13:50 +0200

    Update docker-install.sh

--- jenkins-script/docker-install.sh
+++ jenkins-script/docker-install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sudo apt-get install -y \
+ apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common 
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add - 
-sudo add-apt-repository \
+curl -fsSL https://yum.dockerproject.org/gpg | apt-key add - 
+ add-apt-repository \
     "deb https://apt.dockerproject.org/repo/ \
     ubuntu-$(lsb_release -cs) \
     main" 
-sudo apt-get update
-sudo apt-get -y install docker-engine 
+ apt-get update
+ apt-get -y install docker-engine 
 # add current user to docker group so there is no need to use sudo when running docker
-sudo usermod -aG docker $(whoami)
+ usermod -aG docker $(whoami)
